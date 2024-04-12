32170e76aa28418fd2103e0593bc50d8b0a6d075
josedeneto <josedenetojs@gmail.com>
2024-04-12 15:52:32 +0100

    Primeiro commit

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+
+import nlwLogo from '../assets/logo.svg'
+export function Header(){
+    return(
+        <div className="header">
+        <img src={nlwLogo} alt="logo-nlw" />
+      </div>
+    )
+}
+
